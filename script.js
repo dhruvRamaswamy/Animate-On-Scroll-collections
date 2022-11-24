36242307059c4af59da2308e03389dbae868c8fb
Dhruv Ramaswamy <dhruv.ramaswamy@gmail.com>
2022-11-24 00:20:37 -0500

    Finally! made it reusable. Took way too long. use strict really helped

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-const boxes = document.querySelectorAll('.box')
-let triggerBottom;
-// call checkBoxes() when the window detects a scroll change
-// window.addEventListener('scroll', checkBoxes)
-checkBoxes()
 
+"use strict";
+function BoxContainer(containerIdAttribute){
+    // Selects box items in the given container
+    this.boxes = document.querySelectorAll(`[box-container-id="${containerIdAttribute}"] > .box`)
+    // This is stupid, why do i have to do this fml
+    let localBoxRef = this.boxes
+    this.triggerBottom = window.innerHeight * 6 / 7
+    // Check boxes function: goes through each box, checks to see if they are in position and applies the proper class
+    this.checkBoxes = function() {
+        this.triggerBottom = window.innerHeight * 6 / 7
+        localBoxRef.forEach(box => {
+            // Getboundingclientrect gives back properties of a given element about its position, search it up its not that complicated
 
-function checkBoxes(){
-    // Multiply by 4/5 to account for space at top
-    triggerBottom = window.innerHeight * 6 / 7
-    boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
-        // t
-        const boxBottom = box.getBoundingClientRect().bottom + triggerBottom * 6 / 7
-        // console.log(boxTop, box)
-        if(boxTop < triggerBottom && boxBottom > triggerBottom){
-            box.classList.add('show')
-        }
-        else {
-            box.classList.remove('show')
-        }
+            // Box top is the space between the element and the top of the viewport, same for boxBottom (but bottom...)
+            const boxTop = box.getBoundingClientRect().top
+            // the stuff at the end of this line is to account for when the box should show up, slightly before its in view
+            const boxBottom = box.getBoundingClientRect().bottom + this.triggerBottom * 6 / 7
+
+            // Making sure its in range
+            if(boxTop < this.triggerBottom && boxBottom > this.triggerBottom){
+                box.classList.add('show')
+            }
+            else {
+                box.classList.remove('show')
+            }
+        })
+    }
+    // For each object you initialize this
+    // Does this one time at the start to intialize everything
+    window.addEventListener('scroll', this.checkBoxes)
+    this.checkBoxes()
+}
+
+function initializeBoxes() {
+    const boxContainersElements = Array.from(document.querySelectorAll('.container-boxes'))
+
+    // All you have to do is pass in the name of the container and it will do the rest
+    boxContainersElements.forEach(givenBoxContainer => {
+        let x = new BoxContainer(givenBoxContainer.getAttribute('box-container-id'))
     })
 }
 
-// function BoxContainer(containerItem){
-//     this.boxes = document.querySelectorAll(`${containerItem} > .box`)
-//     this.checkBoxes = function() {
 
-//     }
-// }
+// There: abstracted into a simple function
+initializeBoxes()
